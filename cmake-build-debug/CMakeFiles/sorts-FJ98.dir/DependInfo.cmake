4bc0a0ecd26b593a9510cea1ed8867db0de43694
FJ98 <felix.solano@utec.edu.pe>
2019-08-31 15:53:13 -0500

    Completed and without warnings

---
+++ cmake-build-debug/CMakeFiles/sorts-FJ98.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/felix/GitHubs-2019-2/ADS-20192/sorts-FJ98/main.cpp" "/home/felix/GitHubs-2019-2/ADS-20192/sorts-FJ98/cmake-build-debug/CMakeFiles/sorts-FJ98.dir/main.cpp.o"
+  "/home/felix/GitHubs-2019-2/ADS-20192/sorts-FJ98/mocker.cpp" "/home/felix/GitHubs-2019-2/ADS-20192/sorts-FJ98/cmake-build-debug/CMakeFiles/sorts-FJ98.dir/mocker.cpp.o"
+  "/home/felix/GitHubs-2019-2/ADS-20192/sorts-FJ98/tester.cpp" "/home/felix/GitHubs-2019-2/ADS-20192/sorts-FJ98/cmake-build-debug/CMakeFiles/sorts-FJ98.dir/tester.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sorts-FJ98.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sorts-FJ98.dir/main.cpp.o"
+  "CMakeFiles/sorts-FJ98.dir/mocker.cpp.o"
+  "CMakeFiles/sorts-FJ98.dir/tester.cpp.o"
+  "sorts-FJ98.pdb"
+  "sorts-FJ98"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sorts-FJ98.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
